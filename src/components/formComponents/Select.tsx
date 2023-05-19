b9807fcbf5ef30a3727d5727fd8f98bbed1f4d90
JB-Ogot <jbethwel21@gmail.com>
2023-05-19 11:06:00 +0300

    update select

--- src/components/formComponents/Select.tsx
+++ src/components/formComponents/Select.tsx
@@ -1,4 +1,4 @@
-import { FC, Fragment } from "react";
+import { FC, Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
@@ -14,12 +14,21 @@ interface SelectProps {
 }
 
 export const Select: FC<SelectProps> = ({ options, label, handleChange }) => {
+  const [selected, setSelected] = useState<string>("");
+
+  const handleSelect = (option: Option) => {
+    handleChange(option);
+    setSelected(option.label);
+  };
+
   return (
     <div className="w-72 pl-4">
-      <Listbox onChange={handleChange}>
+      <Listbox onChange={handleSelect}>
         <div className="relative mt-1">
           <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-            <span className="block truncate">{label}</span>
+            <span className="block truncate">
+              {selected ? selected : label}
+            </span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon
                 className="h-5 w-5 text-gray-400"
